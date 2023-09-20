2f3c2b3c4285e63736b544544f13bb3dd0219fa7
ajaymahajan007 <ajaymahajan5033@gmail.com>
2023-09-20 13:01:24 +0530

    assigement
    assigement

---
+++ 01_varibleAssign.js
@@ -0,0 +1,22 @@
+var myFullName
+console.log(myFullName);
+myFullName="Ajay Mahajan";
+console.log(myFullName);
+myFullName='Ajay Dattatray Mahajan';
+console.log(myFullName);
+var mylove="I Love Only JS";
+console.log(mylove);
+var firstName="Ajay";
+console.log("First Name:",firstName);
+var lastName="mahajan";
+console.log("Last Name:",lastName);
+var age="21";
+console.log("Age:",age);
+var address="At-malkhed";
+console.log("Address:",address);
+var pin="Pincode:";
+console.log(pin,"415539");
+var gender="Male";
+console.log("gender:",gender);
+var marr="NA";
+console.log("Married:",marr);
