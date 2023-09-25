5c451e7baea93ad95a397d7aca89891651a7b00a
ajaymahajan007 <ajaymahajan5033@gmail.com>
2023-09-25 18:09:52 +0530

    Assingment 4
    Assingment 4

---
+++ 03_funAssign02.js
@@ -0,0 +1,5 @@
+function bankDetails(bankName,accountNo,location,pinCode){
+    console.log("Bank Name:",bankName,",AccountNO:",accountNo,",location:",location,",pincode",pinCode);
+}bankDetails("CITI BANK",3456782345,"pune  ",431202);
+bankDetails("Axis Bank",7556782345,"mumbai",441202);
+bankDetails("HDFC Bank",8956782345,"pune  ",631202);
---
+++ 03_stingBasicAssign.js
@@ -0,0 +1,13 @@
+console.log("_________NO 1__________");
+function stringBasic() {
+    console.log("My Dream Comany Name Is :Infoses");
+}stringBasic()
+console.log("_________NO 2__________");
+var hobby1="Reading";
+var hooby2="Programming";
+var hooby3="Watching Movie and Web Series";
+console.log("my hobbies include are",hobby1,",",hooby2,",",hooby3);
+console.log("......Include Spaces.....");
+console.log("hobby 1:",hobby1.length);
+console.log("hobby 2:",hooby2.length);
+console.log("hobby 3:",hooby3.length);
