faa4c6ba8a4dbd90690f8197c2c49de7a0f07337
ajaymahajan007 <ajaymahajan5033@gmail.com>
2023-10-10 15:41:13 +0530

    assigment
    ajaajaj

---
+++ 01_varible.js
@@ -0,0 +1,20 @@
+// var ajay;
+// console.log(ajay);
+//  ajay="13";
+// console.log(ajay);
+// ajay='15';
+// console.log("roll no:", 'grede a',ajay);
+// var num1="100";
+// var num2="200";
+// console.log(num1,num2);
+// var temp=num1;
+// num1=num2;
+// num2=temp;
+// console.log(num1,num2);
+var num1=200;
+var num2=300;
+temp=num1;
+num1=num2;
+num2=temp;
+console.log(num1,num2);//swap programm using third varible
+
