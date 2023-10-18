dc1dc559e1b16f672e071f0dbe29fb7f2fa5497a
ajaymahajan007 <ajaymahajan5033@gmail.com>
2023-10-18 17:58:01 +0530

    implicit assigement 1
    implicit assigement 1

---
+++ 05_implicitAssign.js
@@ -0,0 +1,16 @@
+let result;
+result='3'+2;
+console.log(result);
+console.log(`ans:'2' implicit converted into string`);
+console.log(`_______________________________________________`);
+result='3'+true;
+console.log(result);
+console.log(`ans:'true' implicit converted into string`);
+console.log(`_______________________________________________`);
+result='3'+undefined;
+console.log(result);
+console.log(`ans:'Undefined' implicit converted into string`);
+console.log(`_______________________________________________`);
+result='3'+null;
+console.log(result);
+console.log(`ans:'null' implicit converted into string`);
